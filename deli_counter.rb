9f473a74d6ff0bca906885c1ac6d38808fbb9bc8
Flora Petersen <floramarina@comcast.net>
2020-06-04 22:32:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,13 @@
 katz_deli = []
-name_array = []
 
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
+    name_array = []
     katz_deli.each_with_index do |name, index|
       name_array >> "#{index+1}. #{name}"
-
+    name_array.join(" ")
+    puts "The line is currently #{name_array.join(" ")}"
   end
 end# Write your code here.
