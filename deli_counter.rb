60f64ba3ee65bed27affa46ab08ccf5015fac21c
Flora Petersen <floramarina@comcast.net>
2020-06-04 22:59:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,6 @@ end
 
 def take_a_number(katz_deli, newperson)
   katz_deli << newperson
-  katz_deli.each_with_index do |name, index|
-    puts "Welcome #{name}. You are number #{index+1} in line."
+  puts "Welcome #{newperson}. You are number katz_deli.length+1 in line."
   end
 end
