303697498d496fc106ef002f5bbfe0828ee3669f
Flora Petersen <floramarina@comcast.net>
2020-06-04 22:15:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+      puts "The line is currently: #{index+1}. #{name}"
   end
 end# Write your code here.
